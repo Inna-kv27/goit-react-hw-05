9c6ed6dac85869885e3c392996fab32a763f5856
Inna-kv27 <inna.kvitka27@gmail.com>
2025-04-17 21:33:12 +0200

    api-key

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,84 @@
 import axios from 'axios';
 
-const API_KEY = '567ce59573ca5c3d96b4d4c85770d133';
+const API_KEY =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NjdjZTU5NTczY2E1YzNkOTZiNGQ0Yzg1NzcwZDEzMyIsIm5iZiI6MTc0NDg5NTU3OC42MTYsInN1YiI6IjY4MDBmZTVhZWY1YWU2ODdjYmQ5ZTNmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pb0omgZ52vNxPHO37exdiL6AOvtMqCjWJFsRfZTzwZ4';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
-axios.defaults.headers.common['Authorization'] = `Bearer ${API_KEY}`;
+axios.defaults.headers.common[
+  'Authorization'
+] = `Bearer ${API_KEY}`;
 
 export const fetchTrendingMovies = async () => {
   try {
-    const response = await axios.get(`${BASE_URL}/trending/movie/day`);
+    const response = await axios.get(
+      `${BASE_URL}/trending/movie/day`
+    );
     return response.data.results;
   } catch (error) {
+    console.error('Error fetching trending movies:', error);
+    throw error;
+  }
+};
+
+export const searchMovies = async (query) => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/search/movie?query=${query}&include_adult=false`
+    );
+    return response.data.results;
+  } catch (error) {
+    console.error(
+      `Error searching movies with query "${query}":`,
+      error
+    );
+    throw error;
+  }
+};
+
+export const fetchMovieDetails = async (movieId) => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/movie/${movieId}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(
+      `Error fetching details for movie ID ${movieId}:`,
+      error
+    );
+    throw error;
+  }
+};
+
+export const fetchMovieCredits = async (movieId) => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/movie/${movieId}/credits`
+    );
+    return response.data.cast;
+  } catch (error) {
+    console.error(
+      `Error fetching credits for movie ID ${movieId}:`,
+      error
+    );
+    throw error;
+  }
+};
+
+export const fetchMovieReviews = async (movieId) => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/movie/${movieId}/reviews`
+    );
+    return response.data.results;
+  } catch (error) {
+    console.error(
+      `Error fetching reviews for movie ID ${movieId}:`,
+      error
+    );
+    throw error;
+  }
+};
+
+export { IMAGE_BASE_URL };
