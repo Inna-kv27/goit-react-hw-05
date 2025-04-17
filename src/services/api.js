94e98a7ea966beba1f06a8a81b467fc3fc3424f8
Inna-kv27 <inna.kvitka27@gmail.com>
2025-04-17 20:09:41 +0200

    next

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'YOUR_API_KEY';
+const API_KEY = '567ce59573ca5c3d96b4d4c85770d133';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
